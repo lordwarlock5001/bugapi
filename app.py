d55d2c8303f806df7b640372b2460db8bb88635e
Rushikesh Kamble <65526768+lordwarlock5001@users.noreply.github.com>
2021-01-29 10:29:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import flask
 import psycopg2
 from flask import request
 import hashlib
+import json
 app = flask.Flask(__name__)
 
 @app.route('/log',methods=['POST'])
@@ -63,10 +64,10 @@ def register():
     try:
         db_cursor.execute(insert_query, insert_values)
         chat_db.commit()
-        return "success"
+        return json.load('{"task":"succes"}')
     except:
         #print("Error while inserting the new record :", repr(e))
-        return "failure"
+        return json.load('{"task":"failure"}')
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -79,9 +80,9 @@ def login():
     records = db_cursor.fetchall()
 
     if len(records) == 0:
-        return "failure"
+        return json.load('{"task":"failure"}')
     else:
-        return "success"
+        return json.load('{"task":"succes"}')
 
 
 @app.route('/location', methods=['POST'])
@@ -97,10 +98,10 @@ def location():
     try:
         db_cursor.execute(insert_query, insert_values)
         chat_db.commit()
-        return "success"
+        return json.load('{"task":"succes"}')
     except:
         #print("Error while inserting the new record :", repr(e))
-        return "failure"
+        return json.load('{"task":"failure"}')
 
 try:
     chat_db = psycopg2.connect(host="ec2-54-211-55-24.compute-1.amazonaws.com",database="d5vg3bqvsednid",user="cyrvcmwerwegek",password="b96dd8be6079e019632c13464a7e483645a2bedf5d191536b35f4c5e66d08366",port="5432")
