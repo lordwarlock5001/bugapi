f3e48bf7777d50cef2ef889c03b4be4b4e64b810
lordwarlock5001 <65526768+lordwarlock5001@users.noreply.github.com>
2021-06-12 18:10:34 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker,session
 
-SQLALCHEMY_DATABASE_URL = "postgres://stzaojmgcbyfjf:7bbb12c78fd5cb689bc8f71f3b264b6864575c62a917731519d72de0db038fe2@ec2-3-231-69-204.compute-1.amazonaws.com:5432/dfth4qdkruacod"
+SQLALCHEMY_DATABASE_URL = "postgres://uxlliftdzeeaxb:f9115df1505c5c3639700df62276028783cc80b4a3adb4c096f791b8413c4f88@ec2-34-193-112-164.compute-1.amazonaws.com:5432/dfbuo5b2rlhnnp"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
